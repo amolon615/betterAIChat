8dfa15980abce8b963fdcf81148a5143f4712962
amolon615 <114776102+amolon615@users.noreply.github.com>
2023-02-04 22:46:16 +0100

    Initial Commit

---
+++ betterAIChat/betterAIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterAIChatApp.swift
+//  betterAIChat
+//
+//  Created by Artem on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct betterAIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
