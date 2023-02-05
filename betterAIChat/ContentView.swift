04d04758102c4f51bf9928102e02c0d231e97213
amolon615 <114776102+amolon615@users.noreply.github.com>
2023-02-05 16:25:57 +0100

    changes

--- betterAIChat/ContentView.swift
+++ betterAIChat/ContentView.swift
@@ -6,18 +6,72 @@
 //
 
 import SwiftUI
+import ChatGPTSwift
+
+
 
 struct ContentView: View {
+    let api = ChatGPTAPI(apiKey: "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")
+    
+    
+    @State var prompt: String = ""
+    @State var response: [String] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            //Input field
+            TextField(text: $prompt) {
+                Text("Type your request")
+                    .foregroundColor(.white)
+            }
+            .frame(width: 300, height: 50)
+            .background(.gray)
+            .cornerRadius(10)
+            .foregroundColor(.white)
+        
+            
+            //submit button
+            Button{
+                Task {
+                    do {
+                        let stream = try await api.sendMessageStream(text: prompt)
+                        for try await line in stream {
+                            response.append(line)
+                            print(line)
+                        }
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+            } label: {
+                Text("Submit prompt")
+            }
+            .frame(width: 150, height: 50)
+            .background(.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            
+            //Clear button
+            Button{
+                response.removeAll()
+                prompt = ""
+            } label: {
+                Text("Clear")
+            }
+            .frame(width: 150, height: 50)
+            .background(.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            
+            //output field
+            Text(response.joined())
+                .textSelection(.enabled)
         }
         .padding()
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
